c6c3073b977862d28c835bc6d9162cfaaf1f9785
Roshan Bhattarai <45297269+bad-logic@users.noreply.github.com>
2023-05-18 23:47:10 +0545

    using guards and strategies

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+  Inject,
+  Get,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    @Inject(AuthService)
+    private readonly authService: AuthService,
+  ) {}
+
+  @Post('login')
+  @UseGuards(AuthGuard('local'))
+  async login(@Request() request) {
+    return {
+      userId: request.user.id,
+      token: this.authService.getTokenForUser(request.user),
+    };
+  }
+
+  @Get('profile')
+  @UseGuards(AuthGuard('jwt'))
+  async getProfile(@Request() request) {
+    return request.user;
+  }
+}
