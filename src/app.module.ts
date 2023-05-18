56fc1e43bd296773ba2e219f3a88dd4cd1330d36
Roshan Bhattarai <roshanbhattarai@lftechnology.com>
2023-05-18 23:46:37 +0545

    folder restructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EventsModule } from './events/events.module';
-import { ConfigModule } from '@nestjs/config';
+import { EventsModule } from './modules/events/events.module';
 import ormConfig from './config/orm.config';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [ormConfig],
-    }),
-    TypeOrmModule.forRootAsync({
-      useFactory: ormConfig,
+    TypeOrmModule.forRoot({
+      ...ormConfig(),
     }),
     AuthModule,
     EventsModule,
