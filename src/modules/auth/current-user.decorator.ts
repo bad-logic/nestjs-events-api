252a22c7530a1bc0ba2fb28adafe94cc6f02a547
Roshan Bhattarai <roshanbhattarai@lftechnology.com>
2023-06-11 20:41:13 +0545

    currentuser decorator for both gql and rest api

--- src/modules/auth/current-user.decorator.ts
+++ src/modules/auth/current-user.decorator.ts
@@ -1,7 +1,16 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const currentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
+    const context = GqlExecutionContext.create(ctx);
+
+    // works for graphql
+    if (context) {
+      return context.getContext().req.user ?? null;
+    }
+
+    // works for rest apis
     const request = ctx.switchToHttp().getRequest();
     return request.user ?? null;
   },
