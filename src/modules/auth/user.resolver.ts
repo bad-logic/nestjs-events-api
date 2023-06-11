5778f117ae7065fb6f9d9a18d10dbb2062f45e64
Roshan Bhattarai <roshanbhattarai@lftechnology.com>
2023-06-11 20:41:42 +0545

    add me query for authenticated users

--- src/modules/auth/user.resolver.ts
+++ src/modules/auth/user.resolver.ts
@@ -1,20 +1,36 @@
-import { Resolver, Query, Args, Int } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Args,
+  Int,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Logger, UseGuards } from '@nestjs/common';
+import { currentUser } from './current-user.decorator';
+import { AuthGuardJwtGql } from './auth-guard-jwt.gql';
 
 @Resolver(() => User)
 export class UserResolver {
+  private readonly logger = new Logger(UserResolver.name);
+
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
 
+  @Query(() => User)
+  @UseGuards(AuthGuardJwtGql)
+  public me(@currentUser() user: User): User {
+    return user;
+  }
+
   @Query(() => [User])
   public async users(): Promise<User[]> {
-    return this.userRepository.find({
-      relations: ['profile', 'events', 'attended'],
-    });
+    return this.userRepository.find();
   }
 
   @Query(() => User)
@@ -23,7 +39,24 @@ export class UserResolver {
   ): Promise<User> {
     return this.userRepository.findOneOrFail({
       where: { id },
-      relations: ['profile', 'events', 'attended'],
     });
+  }
+
+  @ResolveField('profile')
+  public async profile(@Parent() user: User) {
+    this.logger.debug(`@ResolveField profile was called`);
+    return user.profile;
+  }
+
+  @ResolveField('events')
+  public async events(@Parent() user: User) {
+    this.logger.debug(`@ResolveField events was called`);
+    return user.events;
+  }
+
+  @ResolveField('attended')
+  public async attended(@Parent() user: User) {
+    this.logger.debug(`@ResolveField attended was called`);
+    return user.attended;
   }
 }
