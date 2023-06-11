a0e71ed214bea947d53433ccc69c9b7a8d32de64
Roshan Bhattarai <roshanbhattarai@lftechnology.com>
2023-06-11 21:58:32 +0545

    refactor

--- src/modules/auth/users.controller.ts
+++ src/modules/auth/users.controller.ts
@@ -8,36 +8,13 @@ import {
 
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller('user')
 export class UserController {
-  constructor(
-    private readonly userService: UserService,
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-  ) {}
+  constructor(private readonly userService: UserService) {}
 
   @Post()
   async create(@Body() user: CreateUserDto) {
-    if (user.password !== user.retypedPassword) {
-      throw new BadRequestException(['Passwords are not identical']);
-    }
-    const existingUser = await this.userRepository.findOne({
-      where: [
-        {
-          username: user.username,
-        },
-        {
-          email: user.email,
-        },
-      ],
-    });
-    if (existingUser) {
-      throw new BadGatewayException(['user already exists']);
-    }
     return this.userService.createUser(user);
   }
 }
