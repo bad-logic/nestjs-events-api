1e69c2f30573b61037fe660d4f1e81dc10d30c0c
Roshan Bhattarai <roshanbhattarai@lftechnology.com>
2023-06-11 20:15:26 +0545

    refactor local strategy to be reused

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,16 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
 import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Password } from './password';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  private readonly logger = new Logger(AuthService.name);
+
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
   public getTokenForUser(user: User): string {
     return this.jwtService.sign({
       username: user.username,
       sub: user.id,
     });
+  }
+
+  public async validateUser(username: string, password: string): Promise<User> {
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.username = :username', { username })
+      .addSelect('user.password')
+      .getOne();
+
+    if (!user) {
+      this.logger.log(`user ${username} not found`);
+      throw new UnauthorizedException();
+    }
+    const hash = await new Password(password).compare(user.password);
+
+    if (!hash) {
+      this.logger.log(`user ${username} has wrong credentials`);
+      throw new UnauthorizedException();
+    }
+
+    return user;
   }
 }
--- src/modules/auth/local.strategy.ts
+++ src/modules/auth/local.strategy.ts
@@ -1,40 +1,17 @@
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Password } from './password';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   private readonly logger = new Logger(LocalStrategy.name);
 
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-  ) {
+  constructor(private readonly authService: AuthService) {
     super();
   }
 
   public async validate(username: string, password: string): Promise<any> {
-    const user = await this.userRepository
-      .createQueryBuilder('user')
-      .where('user.username = :username', { username })
-      .addSelect('user.password')
-      .getOne();
-
-    if (!user) {
-      this.logger.log(`user ${username} not found`);
-      throw new UnauthorizedException();
-    }
-    const hash = await new Password(password).compare(user.password);
-
-    if (!hash) {
-      this.logger.log(`user ${username} has wrong credentials`);
-      throw new UnauthorizedException();
-    }
-
-    return user;
+    return this.authService.validateUser(username, password);
   }
 }
